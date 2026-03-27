e067fc04d046d4126392e3f9558368a72b804cce
evetlovelp99 <evetlzp99@gmail.com>
2026-03-27 13:45:32 -0700

    lab7 fix

--- src/main/java/Swimmer.java
+++ src/main/java/Swimmer.java
@@ -1,25 +1,59 @@
 import java.util.List;
 
-public class Swimmer {
-  private String name;
-  private List<Double> butterfly50mTimes;
-  private List<Double> freestyle50mTimes;
+public class Swimmer implements Comparable<Swimmer> {
 
-  public Swimmer(String name, List<Double> butterfly50mTimes, List<Double> freestyle50mTimes) {
-    this.name = name;
-    this.butterfly50mTimes = butterfly50mTimes;
-    this.freestyle50mTimes = freestyle50mTimes;
-  }
+    private String name;
+    private List<Double> butterfly50mTimes;
+    private List<Double> backstroke50mTimes;
+    private List<Double> breaststroke50mTimes;
+    private List<Double> freestyle50mTimes;
 
-  public String getName() {
-    return name;
-  }
+    public Swimmer(String name,
+        List<Double> butterfly50mTimes,
+        List<Double> backstroke50mTimes,
+        List<Double> breaststroke50mTimes,
+        List<Double> freestyle50mTimes) {
+        this.name = name;
+        this.butterfly50mTimes = butterfly50mTimes;
+        this.backstroke50mTimes = backstroke50mTimes;
+        this.breaststroke50mTimes = breaststroke50mTimes;
+        this.freestyle50mTimes = freestyle50mTimes;
+    }
 
-  public List<Double> getButterfly50mTimes() {
-    return butterfly50mTimes;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public List<Double> getFreestyle50mTimes() {
-    return freestyle50mTimes;
-  }
+    public List<Double> getButterfly50mTimes() {
+        return butterfly50mTimes;
+    }
+
+    public List<Double> getBackstroke50mTimes() {
+        return backstroke50mTimes;
+    }
+
+    public List<Double> getBreaststroke50mTimes() {
+        return breaststroke50mTimes;
+    }
+
+    public List<Double> getFreestyle50mTimes() {
+        return freestyle50mTimes;
+    }
+
+    private double averageFreestyle() {
+        if (freestyle50mTimes == null || freestyle50mTimes.isEmpty()) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        double sum = 0.0;
+        for (Double time : freestyle50mTimes) {
+            sum += time;
+        }
+        return sum / freestyle50mTimes.size();
+    }
+
+    @Override
+    public int compareTo(Swimmer otherSwimmer) {
+        return Double.compare(this.averageFreestyle(), otherSwimmer.averageFreestyle());
+    }
 }
--- src/test/java/IteratorTest.java
+++ src/test/java/IteratorTest.java
@@ -2,38 +2,46 @@ import java.util.Arrays;
 import java.util.List;
 
 public class IteratorTest {
-  public static void main(String[] args) {
-    Swimmer swimmer1 = new Swimmer(
-        "Alice",
-        Arrays.asList(30.1, 30.2, 30.3, 30.4, 30.5),
-        Arrays.asList(26.5, 26.0, 27.0)
-    );
+    public static void main(String[] args) {
+        Swimmer swimmer1 = new Swimmer(
+            "Alice",
+            Arrays.asList(30.1, 30.2, 30.3, 30.4, 30.5),
+            Arrays.asList(32.1, 32.2),
+            Arrays.asList(33.1, 33.2),
+            Arrays.asList(26.5, 26.0, 27.0)
+        );
 
-    Swimmer swimmer2 = new Swimmer(
-        "Bob",
-        Arrays.asList(31.1, 31.2, 31.3),
-        Arrays.asList(25.9, 26.0)
-    );
+        Swimmer swimmer2 = new Swimmer(
+            "Bob",
+            Arrays.asList(31.1, 31.2, 31.3),
+            Arrays.asList(34.1, 34.2),
+            Arrays.asList(35.1, 35.2),
+            Arrays.asList(25.9, 26.0)
+        );
 
-    Swimmer swimmer3 = new Swimmer(
-        "Cathy",
-        Arrays.asList(29.1, 29.2, 29.3, 29.4, 29.5),
-        Arrays.asList(27.2, 27.5, 28.0)
-    );
+        Swimmer swimmer3 = new Swimmer(
+            "Cathy",
+            Arrays.asList(29.1, 29.2, 29.3, 29.4, 29.5),
+            Arrays.asList(31.1, 31.2),
+            Arrays.asList(32.1, 32.2),
+            Arrays.asList(27.2, 27.5, 28.0)
+        );
 
-    Swimmer swimmer4 = new Swimmer(
-        "David",
-        Arrays.asList(28.1, 28.2, 28.3, 28.4, 28.5),
-        Arrays.asList(26.1, 27.0)
-    );
+        Swimmer swimmer4 = new Swimmer(
+            "David",
+            Arrays.asList(28.1, 28.2, 28.3, 28.4, 28.5),
+            Arrays.asList(30.1, 30.2),
+            Arrays.asList(31.1, 31.2),
+            Arrays.asList(26.1, 27.0)
+        );
 
-    List<Swimmer> swimmers = Arrays.asList(swimmer1, swimmer2, swimmer3, swimmer4);
+        List<Swimmer> swimmers = Arrays.asList(swimmer1, swimmer2, swimmer3, swimmer4);
 
-    SwimmersIterator iterator = new SwimmersIterator(swimmers);
+        SwimmersIterator iterator = new SwimmersIterator(swimmers);
 
-    while (iterator.hasNext()) {
-      Swimmer s = iterator.next();
-      System.out.println(s.getName());
+        while (iterator.hasNext()) {
+            Swimmer s = iterator.next();
+            System.out.println(s.getName());
+        }
     }
-  }
 }
